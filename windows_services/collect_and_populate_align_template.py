ce0368bd379dd33c24b6759bbf948a90393439d8
Lining Song <lsong@Lining-Songs-MacBook-Pro.local>
2017-11-25 17:18:41 +1100

    minor changes

--- windows_services/collect_and_populate_align_template.py
+++ windows_services/collect_and_populate_align_template.py
@@ -26,8 +26,9 @@ white_list = ['WinRM', 'Dhcp']
 ######################################################################
 service_to_template = {
                        # The mapping below means when we discover a WinRM
-                       # service, a device template with the name 'WinRM' 
-                       # will be applied to this device
+                       # service (the key in the map), a device template with 
+                       # the name 'WinRM' (the value on the right hand side) 
+                       # will be applied to this device. 
                        'WinRM': 'WinRM',
                        'Dhcp': 'Dhcp'
                        }
@@ -66,10 +67,12 @@ class localAPI:
         return requests.delete(self._host+uri, headers=self._headers, auth=self._httpauth, json=payload, verify=False)
     def head(self, uri, payload=None):
         return requests.head(self._host+uri, headers=self._headers, auth=self._httpauth, json=payload, verify=False)
+    
+    # Applying a device template to a device must have a content-type:application/em7-resource-uri
     def apply(self, uri, payload=None):
         return requests.post(self._host+uri, headers={"content-type": "application/em7-resource-uri"}, auth=self._httpauth, data=payload, verify=False)
          
-         
+# Convert template name to template id         
 def get_template_id(dbc, template_name):
     id = dbc.autofetch_value("SELECT template_id FROM master_dev.template_common where template_name = '{0}'".format(template_name))
     return str(id)
@@ -84,7 +87,6 @@ Request = namedtuple('Request', 'req_id key_column request app_id')
 dbc = local_db()
 api = localAPI(dbc)
 
-# Get the data
 try:
     request = Request(-1, 'DisplayName', ps_request, self.app_id)    
     data, error = powershell_winrm(self.did, self.ip, request, self.cred_details, True, None, None, self.logger)
@@ -93,39 +95,35 @@ try:
         self.logger.ui_debug("Error: %s" % (error))
         
     elif data is not None:
-        self.logger.debug("Larry: data is not None ")
         # Rearrange
         # First extract the individual dictionaries
         n = data['Name']
     
         # These are the lists that will be plugged into result_handler['oid_name']
         name = []
+        
         windows_services = '|'
        
         for key in n.keys():
             tmpl = service_to_template.get(n[key], None)
-            self.logger.debug("Larry service_to_template name: %s -> %s" % (n[key], tmpl))
+            self.logger.debug("Service_to_template name: %s -> %s" % (n[key], tmpl))
             if tmpl is not None:
                 tid = get_template_id(dbc, tmpl)
-                self.logger.ui_debug("Larry - tmpl to tid: %s -> %s" % (tmpl, tid))
+                self.logger.ui_debug("Template name to id: %s -> %s" % (tmpl, tid))
                 if tid:
                     try:
                         r = api.apply("/api/device/%s" % (self.did), "/api/device_template/%s" % (tid))
                     except Exception, err:
-                        self.logger.debug("Larry - Error while applying templates to device: %s" % (err))
+                        self.logger.debug("Error while applying templates to device: %s" % (err))
             # generate the list anyway, to show all the services.
             name.append((key, n[key]))
             if white_list and n[key] in white_list:
                 windows_services = windows_services + n[key] + '|'
             elif not white_list:
                 windows_services = windows_services + n[key] + '|'
-           
-        self.logger.ui_debug("Larry - name: %s" % (name))
+        
         # return the transformed data
         result_handler['Name'] = name
-
-        #print "windows_services:"
-        pprint.pprint(windows_services)
         
         custom_attribute = {'c-windows_services': windows_services}
 
@@ -133,4 +131,4 @@ try:
      
 except Exception, err:
     self.internal_alerts.append((COLLECT_ERROR, "%s [%s] for device %s [%s] IP: %s Error: %s" % (app_name, self.app_id, self.name, self.did, self.ip, err)))
-    self.logger.debug("Larry - Error: %s" % (err))
+    self.logger.debug("Error: %s" % (err))
