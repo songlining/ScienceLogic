3cbf8c9f2e977d090d9089476a473130294c9b52
Larry Song <songlining@gmail.com>
2021-02-08 21:46:32 +1100

    cut down version, without plotting

--- perf_data_generator/curve_gen.py
+++ perf_data_generator/curve_gen.py
@@ -8,26 +8,6 @@ def normfun(x,mu,sigma):
     pdf = np.exp(-((x - mu)**2)/(2*sigma**2)) / (sigma * np.sqrt(2*np.pi))
     return pdf
 
-# this function is for testing purpose, to generate a list of datetime points
-def generate_datetime(start, end, interval):
-    # sample start: 2019-01-01 10:00
-    date, time = start.split()
-    year, month, day = date.split("-")
-    hour, minute = time.split(":")
-    d1 = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute))
-
-    date, time = end.split()
-    year, month, day = date.split("-")
-    hour, minute = time.split(":")
-    d2 = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute))
-    delta = datetime.timedelta(minutes=interval)
-    times = []
-    while d1 < d2:
-        times.append(d1)
-        d1 += delta
-    times.append(d2)
-    return times
-
 def get_value(dt, mean, std):
     year, month, day, hour, minute = map(int, dt.strftime("%Y:%m:%d:%H:%M").split(":"))
     minute_of_day = hour * 60 + minute
@@ -41,12 +21,6 @@ def get_value(dt, mean, std):
 
 #################### MAIN ####################
 
-# Set to True for plotting, False for production
-TEST = False
-
-if TEST is True:
-    import matplotlib.pyplot as plt
-    dt_list = generate_datetime("2019-01-01 00:00", "2019-01-14 00:00", 5)
 
 busyhour_start = "09:00"
 busyhour_end = "17:00"
@@ -58,25 +32,6 @@ mean = ((be_h * 60 + be_m) - (bs_h * 60 + bs_m)) / 2 + (bs_h * 60 + bs_m)
 std = ((be_h * 60 + be_m) - (bs_h * 60 + bs_m)) / 2 / 2
 
 
-if TEST is True:
-    x_axis = []
-    y1_axis = []
-    y2_axis = []
-    for dt in dt_list:
-        year, month, day, hour, minute = map(int, dt.strftime("%Y:%m:%d:%H:%M").split(":"))
-        minute_of_day = hour * 60 + minute
-        x = day*24*60 + hour * 60 + minute
-        y = get_value(dt, mean, std)
-        x_axis.append(x)
-        y1_axis.append(y + gauss(30, 15))
-        y2_axis.append(y*2 + gauss(100, 50))
-
-    # print(x_axis)
-    # print(y_axis)
-    plt.plot(x_axis, y1_axis)
-    plt.show()
-
-else:
-    y = get_value(datetime.datetime.now(), mean, std)
-    result_handler["val01"] = [(0, math.floor(y + gauss(30, 15)))]
-    result_handler["val02"] = [(0, math.floor(y*2 + gauss(100, 50)))]
+y = get_value(datetime.datetime.now(), mean, std)
+result_handler["val01"] = [(0, math.floor(y + gauss(30, 15)))]
+result_handler["val02"] = [(0, math.floor(y*2 + gauss(100, 50)))]
