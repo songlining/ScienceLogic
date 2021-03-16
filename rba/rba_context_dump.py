5b32e7aae7c3a3c6680bd5c8f5fd79be17f00ee3
Larry Song <songlining@gmail.com>
2021-03-16 13:27:32 +1100

    upload rba context dump and load files

---
+++ rba/rba_context_dump.py
@@ -0,0 +1,18 @@
+import json
+
+logger=em7_snippets.logger(filename='/tmp/em7_vars.log')
+
+g = globals()
+em7_vars = {}
+for k in list(g):
+    if 'EM7' in k:
+        em7_vars[k] = g[k]
+try: 
+    j = json.dumps(em7_vars, indent = 4).encode('utf8')
+    logger.debug ("EM7 variable dump starts here: ")
+    logger.debug (">>>" + j + "<<<")
+    logger.debug ("EM7 variable dump stops here.")
+    EM7_RESULT = j
+except Exception as ex:
+    logger.debug ("json dump error: {}. This can be ignored in most cases".format(ex))
+
---
+++ rba/rba_context_load.py
@@ -0,0 +1,11 @@
+# The code block below is for manual triggering of the Action. It will be bypassed if it's triggered by an Automation.
+if 'EM7_VALUES' not in globals():
+    import json
+    with open("./em7_vars.json", 'r') as f:
+        j = json.load(f)
+    for k in j:
+        if 'EM7' in k:
+            vars()[k] = j[k]
+
+    j = None
+    print(locals())
